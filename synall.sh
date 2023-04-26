c3de46f16104e86d2b91417eae4970586f46e4a8
hiepns <hiepnsx@gmail.com>
2023-04-26 11:09:04 +0900

    202304262

--- synall.sh
+++ synall.sh
@@ -6,7 +6,7 @@ SERVERS=$(cat /Users/hiepns/cs/servers.txt)
 
 for SERVER in ${SERVERS[@]}; do
     echo -e "\n------------------ SYNC to ${SERVER} ------------------\n"
-    scp conf.d/rate-limit.conf "$SERVER":/etc/nginx/conf.d/
+#    scp conf.d/rate-limit.conf "$SERVER":/etc/nginx/conf.d/
     scp globals/restrictions.conf "$SERVER":/etc/nginx/globals/
 
     ssh "${SERVER}" << EOF
