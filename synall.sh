fbcfdf207e3c7390232daed97d84459005f737fa
hiepns <hiepnsx@gmail.com>
2023-12-26 12:20:39 +0900

    20231226

--- synall.sh
+++ synall.sh
@@ -8,13 +8,15 @@ for SERVER in ${SERVERS[@]}; do
     echo -e "\n------------------ SYNC to ${SERVER} ------------------\n"
 #    scp conf.d/rate-limit.conf "$SERVER":/etc/nginx/conf.d/
     # scp globals/restrictions.conf "$SERVER":/etc/nginx/globals/
-    scp conf.d/common.conf "$SERVER":/etc/nginx/conf.d/
+#    scp conf.d/common.conf "$SERVER":/etc/nginx/conf.d/
+    scp globals/hide-headers.conf "$SERVER":/etc/nginx/globals/
+    scp nginx.conf "$SERVER":/etc/nginx/nginx.conf
 
     ssh "${SERVER}" << EOF
     echo -e "\n"
     hostname
     date
-    nginx -t && nginx -s reload
+    nginx -t
     echo -e "\n"
 EOF
 done
