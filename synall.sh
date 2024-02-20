05b3892605aad9d75ccd01686463f8df7af40c45
hiepns <hiepnsx@gmail.com>
2024-02-20 15:49:23 +0900

    Edit robots.txt

--- synall.sh
+++ synall.sh
@@ -7,10 +7,10 @@ SERVERS=$(cat /Users/hiepns/cs/servers.txt)
 for SERVER in ${SERVERS[@]}; do
     echo -e "\n------------------ SYNC to ${SERVER} ------------------\n"
 #    scp conf.d/rate-limit.conf "$SERVER":/etc/nginx/conf.d/
-    # scp globals/restrictions.conf "$SERVER":/etc/nginx/globals/
+    scp globals/restrictions.conf "$SERVER":/etc/nginx/globals/
 #    scp conf.d/common.conf "$SERVER":/etc/nginx/conf.d/
-    scp globals/hide-headers.conf "$SERVER":/etc/nginx/globals/
-    scp nginx.conf "$SERVER":/etc/nginx/nginx.conf
+#    scp globals/hide-headers.conf "$SERVER":/etc/nginx/globals/
+#    scp nginx.conf "$SERVER":/etc/nginx/nginx.conf
 
     ssh "${SERVER}" << EOF
     echo -e "\n"
